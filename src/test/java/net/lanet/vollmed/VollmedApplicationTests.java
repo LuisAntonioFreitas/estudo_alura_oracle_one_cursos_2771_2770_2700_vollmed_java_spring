5893bc6962ee794cc5e06f526ff0522445066c75
Luis Antonio Freitas <luisantonio.freitas@gmail.com>
2024-06-20 22:58:07 -0300

    commit inicial

---
+++ src/test/java/net/lanet/vollmed/VollmedApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lanet.vollmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VollmedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
