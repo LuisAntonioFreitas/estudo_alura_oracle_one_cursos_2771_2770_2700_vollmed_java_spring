af49123a2101d9d9886df82f08d324d767e55971
Luis Antonio Freitas <luisantonio.freitas@gmail.com>
2024-06-23 23:31:42 -0300

    update das configurações do swagger

--- src/main/java/net/lanet/vollmed/infra/springdocswagger/SpringDocSwaggerConfigurations.java
+++ src/main/java/net/lanet/vollmed/infra/springdocswagger/SpringDocSwaggerConfigurations.java
@@ -6,16 +6,16 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.beans.factory.annotation.Value;
+import net.lanet.vollmed.infra.utilities.ApplicationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringDocSwaggerConfigurations {
 
-    @Value("${api.system-name}") private String apiSystemName;
-    @Value("${api.system-tag-base}") private String apiSystemTagBase;
-    @Value("${api.system-version}") private String apiSystemVersion;
+    @Autowired
+    private ApplicationProperties ap;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -29,9 +29,9 @@ public class SpringDocSwaggerConfigurations {
                                         .bearerFormat("JWT")))
                 // Others
                 .info(new Info()
-                        .title(apiSystemTagBase)
-                        .description(apiSystemName)
-                        .version(apiSystemVersion)
+                        .title(ap.apiSystemTagBase)
+                        .description(ap.apiSystemName)
+                        .version(ap.apiSystemVersion)
                 );
     }
 
