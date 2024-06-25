1e179992601c2f29246aecda9437da67f319e057
Luis Antonio Freitas <luisantonio.freitas@gmail.com>
2024-06-24 21:03:42 -0300

    definindo informações no console sobre inicialização da aplicação

--- src/main/java/net/lanet/vollmed/VollmedApplication.java
+++ src/main/java/net/lanet/vollmed/VollmedApplication.java
@@ -1,15 +1,29 @@
 package net.lanet.vollmed;
 
+import net.lanet.vollmed.infra.utilities.ApplicationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "net.lanet.vollmed")
 public class VollmedApplication {
+	@Autowired
+	private ApplicationProperties ap; //Status
 
 	public static void main(String[] args) {
 		SpringApplication.run(VollmedApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			// Status
+			ap.status("console");
+		};
 	}
 
 }
